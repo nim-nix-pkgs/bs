a4aea050d4a62a68b54d59c6fee57b79596113b2
hugosenari <hugosenari@gmail.com>
2022-03-12 00:54:03 -0300

    chore: re index bs

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''A good alternative to Makefile.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."bs-main".url = "path:./main";
+  inputs."bs-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."bs-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
